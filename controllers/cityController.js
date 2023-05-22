cd027cc52dcbad7a58707a79caccd14c9218beb9
karadagserhat <serhatkaradagg@outlook.com>
2023-05-22 09:47:23 +0300

    Fixed bugs getCity endpoint

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -2,7 +2,7 @@ const City = require('../models/cityModel');
 const factory = require('./handlerFactory');
 
 exports.getAllCities = factory.getAll(City);
-exports.getCity = factory.getOne(City, { path: 'tours' });
+exports.getCity = factory.getOne(City);
 exports.createCity = factory.createOne(City);
 exports.updateCity = factory.updateOne(City);
 exports.deleteCity = factory.deleteOne(City);
