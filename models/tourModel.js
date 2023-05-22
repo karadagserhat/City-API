7f4c2c89164e899adc4727f5adeabdb2f0d6579f
karadagserhat <serhatkaradagg@outlook.com>
2023-05-22 10:15:38 +0300

    Fixed bugs name required in tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "tour can't be empty!"],
+    required: [true, 'A tour must have a name'],
   },
   type: {
     type: String,
